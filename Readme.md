2928688c08e388bf3ede3657006668c4a521bb80
NamitS27 <namit.s@ahduni.edu.in>
2021-06-22 12:33:20 +0530

    get meaning of the word

---
+++ Readme.md
@@ -0,0 +1,37 @@
+## WORDS API
+
+This project helps in fetching the meaning of the word using famous Words API by using its free "Try It" option available on their [website](https://www.wordsapi.com/).
+
+<br>
+
+### Prequisites
+
+-   Download & install chrome web browser. Clich [here](https://www.google.com/chrome/) to download.
+-   Download chrome driver wrt to the version of the chrome installed in your system. Click [here](https://chromedriver.chromium.org/downloads) to download.
+
+> **Note**: You can use any web browser. For changing the web browser, see [here](https://www.selenium.dev/documentation/en/webdriver/).
+
+<br>
+
+### Steps to run the project
+
+-   Clone the repository
+    ```bash
+    git clone https://github.com/NamitS27/WordsAPI.git
+    ```
+-   Install the dependencies using `pip install -r requirements.txt`.
+-   Run `python get_meaning.py` to run the example (getting meaning of word "example").
+
+<br>
+
+### Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Added some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+**Show some ❤️ by starring the repository!**
---
+++ get_meaning.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+import time
+import json
+
+def get_meaning(searchterm):
+    """ 
+    Fetches the meaning of the word specified
+    
+    :param searchterm: the word for which you want to fetch the meaning
+    :return: json object of the meaning 
+    """
+
+    # finds the input field by id in the webpage
+    sbox = driver.find_element_by_id('word')
+    sbox.clear() # clears the input field
+    sbox.send_keys(searchterm) # enters the word specified in the input field
+
+    # find the 'CALL THE API' button 
+    submit = driver.find_element_by_id("getWord")
+    submit.click() # invoking the click event on the button
+
+    # waiting for the results to come
+    time.sleep(1)
+
+    # find the code tag in the webpage where the meaning of the word (result) is present
+    code = driver.find_element_by_tag_name("code")
+
+    # condition if the meaning is not found
+    if code.text == "No results for that word.":
+        return {
+            'word':searchterm
+        }
+
+    # converting the meaning of the word from string to json
+    meaning = json.loads(code.text)
+
+    # returning the meaning of word in json formart
+    return meaning
+
+if __name__=="__main__":
+    # URL for the Words API webpage containing a free (try it) option where one can search meaning of the provided word
+    webpage = r"https://www.wordsapi.com/"
+
+    '''
+    Specifying the particular version of chrome driver for the particular version of the chrome installed in your system
+    You can download chrome from here > https://www.google.com/chrome/
+    You can download the specific chrome driver wrt to the versin of the chrome from here > https://chromedriver.chromium.org/downloads
+    '''
+    options = webdriver.ChromeOptions()
+    chrome_driver_binary = "chromedriver.exe"
+    driver = webdriver.Chrome(chrome_driver_binary)
+
+    # opening the url in chrome
+    driver.get(webpage)
+
+    # Example
+    word = "example" # changes this as per the requirements
+    print(get_meaning(word))
